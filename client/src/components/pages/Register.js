0af2f68c85f5cd8e2f7feb8b73230719f58de99f
Christian Gonzalez <cgonzalez233@yahoo.com>
2021-04-06 19:12:39 -0400

    fixed register div

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { Store } from '../../store';
-import { registerUser, setErrors } from '../../store/actions/authActions';
-import classnames from 'classnames';
+import React, { useEffect, useRef, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Store } from "../../store";
+import { registerUser, setErrors } from "../../store/actions/authActions";
+import classnames from "classnames";
 
-const Register = props => {
+const Register = (props) => {
   const { state, dispatch } = useContext(Store);
   const errors = state.error;
   const nameRef = useRef();
@@ -13,11 +13,10 @@ const Register = props => {
   const password2Ref = useRef();
 
   useEffect(() => {
-    if (state.auth.isAuthenticated)
-      props.history.push('/dashboard');
-  }, [ state, props ]);
+    if (state.auth.isAuthenticated) props.history.push("/dashboard");
+  }, [state, props]);
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     dispatch(setErrors({ response: { data: {} } }));
@@ -34,55 +33,82 @@ const Register = props => {
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col s8 offset-s2">
-          <Link to="/" className="btn-flat waves-effect">
-            <i className="material-icons left">keyboard_backspace</i> Back to home
-          </Link>
-          <div className="col s12" style={{ paddingLeft: '11.250px' }}>
-            <h4>
-              <b>Register</b> below
-            </h4>
-            <p className="grey-text text-darken-1">
-              Already have an account? <Link to="/login">Login</Link>
-            </p>
+      <div id="loginDiv" className="glass">
+        <div className="row">
+          <div className="col s8 offset-s2">
+            <Link to="/" className="btn-flat waves-effect">
+              <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </Link>
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <h4>
+                <b>Register</b> below
+              </h4>
+              <p className="grey-text text-darken-1">
+                Already have an account? <Link to="/login">Login</Link>
+              </p>
+            </div>
+            <form noValidate onSubmit={onSubmit}>
+              <div className="input-field col s12">
+                <input
+                  ref={nameRef}
+                  error={errors.name}
+                  name="name"
+                  type="text"
+                  className={classnames("", { invalid: errors.name })}
+                />
+                <label htmlFor="name">Name</label>
+                <span className="red-text">{errors.name}</span>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  ref={emailRef}
+                  error={errors.email}
+                  name="email"
+                  type="email"
+                  className={classnames("", { invalid: errors.email })}
+                />
+                <label htmlFor="email">Email</label>
+                <span className="red-text">{errors.email}</span>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  ref={passwordRef}
+                  error={errors.password}
+                  name="password"
+                  type="password"
+                  className={classnames("", { invalid: errors.password })}
+                />
+                <label htmlFor="password">Password</label>
+                <span className="red-text">{errors.password}</span>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  ref={password2Ref}
+                  error={errors.password2}
+                  name="password2"
+                  type="password"
+                  className={classnames("", { invalid: errors.password2 })}
+                />
+                <label htmlFor="password2">Confirm Password</label>
+                <span className="red-text">{errors.password2}</span>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem",
+                  }}
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
           </div>
-          <form noValidate onSubmit={onSubmit}>
-            <div className="input-field col s12">
-              <input ref={nameRef} error={errors.name} name="name" type="text"
-                     className={classnames('', { invalid: errors.name })} />
-              <label htmlFor="name">Name</label>
-              <span className="red-text">{errors.name}</span>
-            </div>
-            <div className="input-field col s12">
-              <input ref={emailRef} error={errors.email} name="email" type="email"
-                     className={classnames('', { invalid: errors.email })} />
-              <label htmlFor="email">Email</label>
-              <span className="red-text">{errors.email}</span>
-            </div>
-            <div className="input-field col s12">
-              <input ref={passwordRef} error={errors.password} name="password" type="password"
-                     className={classnames('', { invalid: errors.password })} />
-              <label htmlFor="password">Password</label>
-              <span className="red-text">{errors.password}</span>
-            </div>
-            <div className="input-field col s12">
-              <input ref={password2Ref} error={errors.password2} name="password2" type="password"
-                     className={classnames('', { invalid: errors.password2 })} />
-              <label htmlFor="password2">Confirm Password</label>
-              <span className="red-text">{errors.password2}</span>
-            </div>
-            <div className="col s12" style={{ paddingLeft: '11.250px' }}>
-              <button className="btn btn-large waves-effect waves-light hoverable blue accent-3" style={{
-                width: '150px',
-                borderRadius: '3px',
-                letterSpacing: '1.5px',
-                marginTop: '1rem',
-              }} type="submit">
-                Sign Up
-              </button>
-            </div>
-          </form>
         </div>
       </div>
     </div>
